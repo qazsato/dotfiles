4fba2369840269441b58552af30da583873e9249
qazsato <qazsato@gmail.com>
2018-07-08 11:46:17 +0900

    Add peco and ghq

--- config.fish
+++ config.fish
@@ -1,3 +1,4 @@
+## For bobthefish
 set -g theme_color_scheme dark
 set -g theme_display_git_ahead_verbose yes
 set -g theme_display_git_dirty_verbose yes
@@ -5,4 +6,16 @@ set -g theme_display_git_master_branch yes
 set -g theme_git_worktree_support yes
 set -g theme_display_date no
 
+## For Ruby
 rbenv init - | source
+
+## For peco
+function fish_user_key_bindings
+  bind \cr 'peco_select_history (commandline -b)'
+end
+
+## For ghq
+function fish_user_key_bindings
+  bind \cr 'peco_select_history (commandline -b)'
+  bind \c] peco_select_ghq_repository
+end
