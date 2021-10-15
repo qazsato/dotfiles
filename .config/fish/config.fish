697f09d0dc4c666e4b8866445e278672d1b2092a
qazsato <qazsato@gmail.com>
2021-10-15 09:53:38 +0900

    add dsp command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 alias dcb='docker-compose build'
 alias dcd='docker-compose down'
 alias dcu='docker-compose up -d'
-alias dcp='docker container prune -f & docker image prune -f & docker volume prune -f'
+alias dsp='docker system prune -af'
 
 ## For bobthefish
 set -g theme_color_scheme dark
@@ -32,3 +32,9 @@ end
 
 ## For mysql
 set -g fish_user_paths "/usr/local/opt/mysql@5.7/bin" $fish_user_paths
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[ -f /Users/kazuki-sato/ghq/github.com/qazsato/melt-api/node_modules/tabtab/.completions/serverless.fish ]; and . /Users/kazuki-sato/ghq/github.com/qazsato/melt-api/node_modules/tabtab/.completions/serverless.fish
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[ -f /Users/kazuki-sato/ghq/github.com/qazsato/melt-api/node_modules/tabtab/.completions/sls.fish ]; and . /Users/kazuki-sato/ghq/github.com/qazsato/melt-api/node_modules/tabtab/.completions/sls.fish
--- .zshrc
+++ .zshrc
@@ -1,7 +1,7 @@
 alias dcb='docker-compose build'
 alias dcd='docker-compose down'
 alias dcu='docker-compose up -d'
-alias dcp='docker container prune -f & docker image prune -f & docker volume prune -f'
+alias dsp='docker system prune -af'
 
 # prezto (https://github.com/sorin-ionescu/prezto)
 if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
